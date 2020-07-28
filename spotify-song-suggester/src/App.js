b47467eed83cf56bbd249c6b6c369ec4ef167e14
Alex Kemper <alexkemper@MacBook-Pro.local>
2020-07-27 20:14:33 -0400

    initial dashboard created

--- spotify-song-suggester/src/App.js
+++ spotify-song-suggester/src/App.js
@@ -1,76 +1,73 @@
-import React, {useState} from 'react';
-import {Link, Route} from "react-router-dom"
-import axios from "axios"
+import React, { useState } from "react";
+import { Link, Route } from "react-router-dom";
+import axios from "axios";
 
-import './App.css';
-import LogIn from "./components/LogIn"
-import Register from "./components/Register"
-import Something from "./components/Something"
+import "./App.css";
+import LogIn from "./components/LogIn";
+import Register from "./components/Register";
+import Something from "./components/Something";
+import Dashboard from "./components/Dashboard Components/Dashboard";
 
 const formInitialValue = {
   name: "",
   email: "",
   password: "",
   conPassword: "",
-  terms: false
-}
+  terms: false,
+};
 
-
-const usersInitialValue = []
+const usersInitialValue = [];
 
 function App() {
-  const [form, setForm] = useState(formInitialValue)
-  const [ users, setUsers] = useState(usersInitialValue)
+  const [form, setForm] = useState(formInitialValue);
+  const [users, setUsers] = useState(usersInitialValue);
 
   const formValueHandler = (name, value) => {
-    setForm({...form, [name]: value})
-  }
+    setForm({ ...form, [name]: value });
+  };
 
-
-
-  const postUsers = newUser => {
-    axios.post('https://reqres.in/api/users', newUser)
-    .then(res=>{
-      setForm(formInitialValue)
-      setUsers([...users, res.data])
-      
-    })
-    .catch(()=> console.log('axios.post err'))
-  }
-  
+  const postUsers = (newUser) => {
+    axios
+      .post("https://reqres.in/api/users", newUser)
+      .then((res) => {
+        setForm(formInitialValue);
+        setUsers([...users, res.data]);
+      })
+      .catch(() => console.log("axios.post err"));
+  };
 
   const submit = () => {
-    const newUser ={
+    const newUser = {
       name: form.name.trim(),
       email: form.email.trim(),
       password: form.password.trim(),
-      terms: form.terms
-    }
+      terms: form.terms,
+    };
     //send this information to the function that post to axios
-    postUsers(newUser)
-  }
-  
+    postUsers(newUser);
+  };
+
   return (
     <div>
       <Route exact path="/">
-      Don't have an Acount?
-      <Link to="/Register">Register</Link>
+        Don't have an Acount?
+        <Link to="/Register">Register</Link>
         <LogIn />
       </Route>
 
       <Route path="/Register">
-      ALready have an Acout?
-      <Link to="/">Login</Link>
-        <Register 
-          update={formValueHandler}
-          values={form}
-          submit={submit}
-         />
+        ALready have an Acout?
+        <Link to="/">Login</Link>
+        <Register update={formValueHandler} values={form} submit={submit} />
       </Route>
 
+      <Link to="/dashboard">Dashboard</Link>
 
       <Route path="/Something">
-          <Something />
+        <Something />
+      </Route>
+      <Route path="/dashboard">
+        <Dashboard />
       </Route>
     </div>
   );
---
+++ spotify-song-suggester/src/components/Dashboard Components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//Components
+import DashHeader from "./DashHeader";
+
+const Dashboard = () => {
+  return (
+    <div className="app-wrapper">
+      <DashHeader />
+      <div>Dashboard Component</div>
+    </div>
+  );
+};
+
+export default Dashboard;
