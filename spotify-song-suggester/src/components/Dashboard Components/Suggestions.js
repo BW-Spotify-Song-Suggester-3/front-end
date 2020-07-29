636f3e2f3330a2791d655d011908a859745f182a
Alex Kemper <alexkemper@MacBook-Pro.local>
2020-07-29 01:26:40 -0400

    suggestions call to spotify is now working

--- spotify-song-suggester/src/components/Dashboard Components/Suggestions.js
+++ spotify-song-suggester/src/components/Dashboard Components/Suggestions.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { axiosWithAuthSpotify } from "../utils/axiosWithAuth";
 
 const Suggestions = () => {
   const { id } = useParams();
+  const [song, setSong] = useState(id);
+  const [loading, toggleLoading] = useState(true);
 
-  return (
-    <div>
-      <h1>Suggestions {id}</h1>
-    </div>
-  );
+  useEffect(() => {
+    axiosWithAuthSpotify()
+      .get(song)
+      .then((res) => {
+        console.log("SPOTIFY RESPONSE:", res);
+        setSong(res.data);
+        toggleLoading(false);
+      })
+      .catch((err) => {
+        console.log("SPOTIFY ERROR:", err);
+      });
+  }, []);
+
+  if (loading === true) {
+    return <h1>loading...</h1>;
+  } else {
+    return (
+      <div>
+        <h1>
+          Suggestions based on {song.name} by {song.artists[0].name}
+        </h1>
+        <div className="fav-songs-card">
+          <div key={song.spotifyid} className="song-cards">
+            <img
+              src={song.album.images[0].url}
+              alt={song.album.name}
+              className="album-covers"
+            />
+            <div className="info-container">
+              <div className="info-box">
+                <div className="title">{song.name}</div>
+                <div className="artist">{song.artists[0].name}</div>
+              </div>
+
+              <embed src={song.preview_url} className="song-preview" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Suggestions;
