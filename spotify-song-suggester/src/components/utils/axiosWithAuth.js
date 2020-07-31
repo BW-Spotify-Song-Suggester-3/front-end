a1cf56bac1cabe8f57891c745f2aea722c1c4104
Alex Kemper <alexkemper@MacBook-Pro.local>
2020-07-31 15:55:33 -0400

    spotify token is now called programmatically upon log in :)

--- spotify-song-suggester/src/components/utils/axiosWithAuth.js
+++ spotify-song-suggester/src/components/utils/axiosWithAuth.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const axiosWithAuthSpotify = () => {
-  const token =
-    "BQC9E7-dDVHxmzZ1bQeX_0Cn0zK5WBj41-y5zUUVCNn_DngPh_e9p4aYVKBDDuUManfDgDa8AWuif_PXpds";
+  const token = localStorage.getItem("spotify_token");
   return axios.create({
     baseURL: "https://api.spotify.com/v1/tracks/",
     headers: {
@@ -19,6 +18,23 @@ export const axiosWithAuth = () => {
       Authorization: `Bearer ${token}`,
     },
   });
+};
+
+export const getSpotifyToken = () => {
+  axios
+    .post(
+      "https://accounts.spotify.com/api/token",
+      "grant_type=client_credentials",
+      {
+        headers: {
+          Authorization: `Basic YTFkNTk0MmRkNGU2NGUzYmFmMTYzNDJiNDBjODIxNTE6YThlMjJhNjkwODNmNDljNmFhOWE5Yzk0ZWI5MGM4NTM=`,
+        },
+      }
+    )
+    .then((res) => {
+      console.log("spotify token", res);
+      window.localStorage.setItem("spotify_token", res.data.access_token);
+    });
 };
 
 //curl -X "POST" -H "Authorization: Basic YTFkNTk0MmRkNGU2NGUzYmFmMTYzNDJiNDBjODIxNTE6YThlMjJhNjkwODNmNDljNmFhOWE5Yzk0ZWI5MGM4NTM=" -d grant_type=client_credentials https://accounts.spotify.com/api/token
